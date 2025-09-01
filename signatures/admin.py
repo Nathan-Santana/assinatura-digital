cc4dc7dcdcb80437801233d5ab7cb4a499202412
Nathan-Santana <nathansantana@rede.ulbra.br>
2025-09-01 11:43:30 -0300

    Limpando repositorio de arquivos indesejados

--- signatures/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import User, Signature, VerificationLog
-
-admin.site.register(User)
-admin.site.register(Signature)
-admin.site.register(VerificationLog)
